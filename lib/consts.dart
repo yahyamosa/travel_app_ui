7ae8448b72eef01b8677a7f6f8f430dbb7e52f3d
yahya mosa <yhea23112003@gamil.com>
2023-08-30 10:56:24 +0300

    travel app

--- lib/consts.dart
+++ lib/consts.dart
@@ -118,3 +118,6 @@ const hotels = [
     'price': '195'
   }
 ];
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: Locale(""),
       title: 'Flutter Demo',
       theme: ThemeData(
           outlinedButtonTheme: outlinedButtonThemeData,
